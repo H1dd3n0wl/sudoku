a1e118ab05c501540af821df710211202c594c1f
Zhukov <sviter1232@gmail.com>
2023-08-25 17:20:00 +0300

    added possible numbers

--- SimpleSolver.cpp
+++ SimpleSolver.cpp
@@ -1,19 +1,20 @@
 #include "SimpleSolver.hpp"
 
-SimpleSolver::SimpleSolver(std::vector<std::vector<char>> board) : board_{board} {}
+SimpleSolver::SimpleSolver(std::vector<std::vector<char>> board)
+    : board_{board},
+      possible_numbers(BOARD_SIZE, std::vector<std::set<int>>(
+                                       BOARD_SIZE, {1, 2, 3, 4, 5, 6, 7, 8, 9})) {}
 
 bool SimpleSolver::isValid() {
-    std::vector<int> column_check(9, 0);
-    std::vector<int> row_check(9, 0);
+    std::vector<int> column_check(BOARD_SIZE, 0);
+    std::vector<int> row_check(BOARD_SIZE, 0);
 
-    auto get([](char c) {
-        return (int) (c - '1');
-    });
+    auto get([](char c) { return (int)(c - '1'); });
 
-    for (int i = 0; i < 9; ++i) {
-        column_check.assign(9, 0);
-        row_check.assign(9, 0);
-        for (int j = 0; j < 9; ++j) {
+    for (int i = 0; i < BOARD_SIZE; ++i) {
+        column_check.assign(BOARD_SIZE, 0);
+        row_check.assign(BOARD_SIZE, 0);
+        for (int j = 0; j < BOARD_SIZE; ++j) {
             if (board_[i][j] != '.' && row_check[get(board_[i][j])]++) {
                 return false;
             }
@@ -23,10 +24,10 @@ bool SimpleSolver::isValid() {
         }
     }
 
-    std::vector<int> block_check(9, 0);
+    std::vector<int> block_check(BOARD_SIZE, 0);
     for (int i = 1; i <= 7; i += 3) {
         for (int j = 1; j <= 7; j += 3) {
-            block_check.assign(9, 0);
+            block_check.assign(BOARD_SIZE, 0);
             for (int x = -1; x < 2; ++x) {
                 for (int y = -1; y < 2; ++y) {
                     if (board_[i + x][j + y] == '.') {
--- SimpleSolver.hpp
+++ SimpleSolver.hpp
@@ -3,14 +3,17 @@
 #ifndef SIMPLE_SOLVER
 #define SIMPLE_SOLVER
 
-#include <vector>
+#include <set>
 #include <unordered_map>
+#include <vector>
 
 class SimpleSolver {
 private:
+    constexpr static inline int BOARD_SIZE = 9;
+
     std::vector<std::vector<char>> board_;
 
-    constexpr static inline int BOARD_SIZE = 9;
+    std::vector<std::vector<std::set<int>>> possible_numbers;
 
 public:
 
