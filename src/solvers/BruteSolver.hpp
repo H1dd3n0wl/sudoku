fb3e92eaa8f476fe6a7ba57f5640e8f6de025ea3
H1dd3n0wl <sviter1232@gmail.com>
2023-08-28 00:18:56 +0300

    BruteSolver started, not working yet

--- src/solvers/BruteSolver.hpp
+++ src/solvers/BruteSolver.hpp
@@ -4,6 +4,8 @@
 #define BRUTE_SOLVER
 
 #include <vector>
+#include <ctype.h>
+#include <unordered_set>
 
 class BruteSolver {
 private:
@@ -11,8 +13,38 @@ private:
 
     std::vector<std::vector<char>> board_;
 
+    std::vector<std::vector<std::unordered_set<int>>> possible_nums;
+
+    std::vector<std::vector<std::unordered_set<int>>> updated_cells;
+
+    std::vector<std::vector<bool>> used_dfs;
+
+    std::unordered_set<int> empty_cells;
+
+    std::vector<int> cells_stack;
+
+    char charFromInt(int x) {
+        return (char)(x + '0');
+    }
+    int intFromChar(char c) {
+        return (int)(c - '0');
+    }
+    int getIndex(int x, int y) {
+        return 9 * x + y;
+    }
+    std::pair<int, int> getCoordinates(int ind) {
+        return {ind / 9, ind % 9};
+    }
+
+    bool needToSolve() { return !empty_cells.empty(); }
+
 public:
     BruteSolver(std::vector<std::vector<char>> board);
+
+    void usualUpdate(int row, int col);
+    void hardUpdate(int row, int col, int previous_cell);
+
+    void solve();
     
     ~BruteSolver() {}
 };
